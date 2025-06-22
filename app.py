a7d54fe2e3685779f2903e62729c652380b6e6ed
appsuniverse <ade.suhendri@miraeasset.co.id>
2025-06-22 17:48:55 +0700

    Remove sample tx

--- app.py
+++ app.py
@@ -524,8 +524,6 @@ class BlockchainNode:
         print(f"Node ID: {self.blockchain.node_id}")
         self.app.run(host=self.host, port=self.port, debug=debug, threaded=True)
 
-
-# Contoh penggunaan
 if __name__ == '__main__':
     import sys
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
